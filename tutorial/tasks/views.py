44039d4e5c461e8c7ccf3d77f135b4174de98dcc
abhishekanalytics <abhishekanalytics2023@gmail.com>
2024-03-05 19:49:47 +0530

    work on class based views using mixin and generic

--- tutorial/tasks/views.py
+++ tutorial/tasks/views.py
@@ -1,42 +1,100 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Task
-from .serializers import TasksSerializer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Task
+# from django.http import Http404
+# from .serializers import TasksSerializer
 
 
-@api_view(['GET', 'POST'])
-def task_list(request):
-    if request.method == 'GET':
-        tasks = Task.objects.all()
-        serializer = TasksSerializer(tasks, many=True)
-        return Response(serializer.data)
+# class TaskList(APIView):
+#     def get(self,request,format=None):
+#             tasks = Task.objects.all()
+#             serializer = TasksSerializer(tasks, many=True)
+#             return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = TasksSerializer(data=request.data)  
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self,request,format=None):
+#         serializer = TasksSerializer(data=request.data)  
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def task_detail(request, pk):
-    try:
-        tasks = Task.objects.get(pk=pk)
-    except Task.DoesNotExist:
-        return Response({'massege':'Task Not Found'})
+# class TaskDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Task.objects.get(pk=pk)
+#         except Task.DoesNotExist:
+#             raise Http404
 
-    if request.method == 'GET':
-        serializer = TasksSerializer(tasks)
-        return Response(serializer.data)
+#     def get(self,request,pk,format=None):
+#         tasks=self.get_object(pk)
+#         serializer = TasksSerializer(tasks)
+#         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = TasksSerializer(tasks, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        tasks.delete()
-        return Response({'massege':'Successfully deleted'})
+#     def put(self,request,pk,format=None):
+#         tasks=self.get_object(pk)
+#         serializer = TasksSerializer(tasks, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     def delete(self,request,pk,format=None):
+#         tasks=self.get_object(pk)
+#         tasks.delete()
+#         return Response({'massege':'Successfully deleted'})
+
+
+############## ----------->>>> Using mixins<<<-------------
+
+
+# from tasks.models import Task
+# from tasks.serializers import TasksSerializer
+# from rest_framework import mixins
+# from rest_framework import generics
+
+# class TaskList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TasksSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
+# class TaskDetail(mixins.RetrieveModelMixin,
+#                     mixins.UpdateModelMixin,
+#                     mixins.DestroyModelMixin,
+#                     generics.GenericAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TasksSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+
+# ----------->>>>Using generic class-based views<<<<--------------  
+
+from tasks.models import Task
+from tasks.serializers import TasksSerializer
+from rest_framework import generics
+
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TasksSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TasksSerializer
